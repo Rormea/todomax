e60635d56976144663d4345afec6d6edee93e0eb
Ronald Ormea <rormeasdev@gmail.com>
2025-07-11 01:10:06 +0200

    Creation of the task module. In addition to creating a method in the task service to assess whether the project exists and is active

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,17 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { TaskController } from './task.controller';
-import { Type } from 'class-transformer';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './entities/task.entity';
+import { ProjectsModule } from 'src/project/projects.module';
+
+
 
 @Module({
   controllers: [TaskController],
   providers: [TaskService],
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [
+    TypeOrmModule.forFeature([Task]),
+    ProjectsModule
+],
   // si bien en el app modulo se ha configurado el autoLoadEntities: true,
   // es una buena práctica importar las entidades en cada módulo que las necesite.
   // importante para habilitar la inyección de repositorios en un módulo específico.
+  exports: [TaskService], // Export the service if it needs to be used in other modules
 
 })
 export class TaskModule {}
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Task } from './entities/task.entity';
+import { Repository } from 'typeorm';
+import { ProjectsService } from '../project/projects.service';
 
 @Injectable()
 export class TaskService {
+
+  private readonly logger = new Logger('TaskService'); 
+
+    constructor(
+      @InjectRepository(Task)
+      private readonly tasksRepository: Repository<Task>,
+      private readonly projectsService: ProjectsService
+    ){}
+
+
   create(createTaskDto: CreateTaskDto) {
     return 'This action adds a new task';
   }
